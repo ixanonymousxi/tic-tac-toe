d268b1b485c50a2f6f046b72da80ba4261620fb8
Sarah <louise562@aol.com>
2023-03-12 12:03:33 -0500

    Added sturctural files. Added HTML & CSS Skeleton structure. Added javascript pseudocode .

---
+++ script.js
@@ -0,0 +1,19 @@
+const GameBoard = () => {
+    //Module
+
+    //Function to display board to window
+    //Function to store board
+    //Function to get the board state
+    //Function to update the board state
+
+    //Function to check end game state
+    //Function to end game results.
+}
+
+
+const Player = () => {
+    //Factory Function
+
+    //Function to initiate a player with name and symbol
+    //Function to interact with the board on turn
+}
